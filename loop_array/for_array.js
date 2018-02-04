9ab9008e77f53ab64c9370cd4b0589c2f90e36ac
cag2050 <cag2050@163.com>
2018-02-04 21:54:26 +0800

    m

---
+++ loop_array/for_array.js
@@ -0,0 +1,5 @@
+let arr = ['a', 'b', 'c']
+let len = arr.length
+for (let i = 0; i < len; i++) {
+    console.log(arr[i])
+}
---
+++ loop_object/for_in_object.js
@@ -0,0 +1,8 @@
+let obj = {
+    a: 'aaa',
+    b: 'bbb',
+    c: 'ccc'
+}
+for (let key in obj) {
+    console.log(key)
+}
